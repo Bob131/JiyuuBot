0defec8b57d11085580c1fc57b07d4580768215b
Bob131 <bob@bob131.so>
2013-10-19 22:33:42 +1100

    More stuff

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import ssl
 import os
 import urllib2
 import thread
+import time
 
 MPD_HOST = "127.0.0.1"
 MPD_PORT = 6600
@@ -32,16 +33,25 @@ if not os.path.exists(os.path.join(MUSIC_PATH, NICK+"_downloaded_music")):
     os.mkdir(os.path.join(MUSIC_PATH, NICK+"_downloaded_music"))
 
 
-def parse_command(command):
+def parse_command(command, what):
     if command.startswith("download") or command.startswith("dl"):
-        args = command[command.index(" ")+1:]
-        return_output("Fetching track "+args)
-        track = open(os.path.join(os.path.join(MUSIC_PATH, NICK+"_downloaded_music"), args[args.rindex("/")+1:]), "wb")
-        dl = urllib2.urlopen(args)
-        track.write(dl.read())
-        track.close()
-        return_output(args+" fetched")
-        mpc.update()
+        try:
+            args = command[command.index(" ")+1:]
+            return_output("Fetching track "+args)
+            dl = urllib2.urlopen(args)
+            if dl.info().maintype == "audio":
+                track = open(os.path.join(os.path.join(MUSIC_PATH, NICK+"_downloaded_music"), args[args.rindex("/")+1:]), "wb")
+                track.write(dl.read())
+                track.close()
+                mpc.update()
+                time.sleep(5)
+                mpc.searchadd("file", args[args.rindex("/")+1:])
+                return_output(args+" fetched and queued")
+            else:
+                return_output("Mime type "+dl.info().type+" not allowed.")
+        except Exception, e:
+            return_output(e)
+            return_output("I dun goofed, soz aye m80.")
     elif command.startswith("add"):
         args = command[4:]
         mpc.searchadd("any", args)
@@ -58,10 +68,8 @@ def parse_command(command):
         return_output(queue)
     elif command == "stats":
         return_output(mpc.stats())
-    elif command == "clear":
-        mpc.clear()
     elif command.startswith("help"):
-        return_output("Available commands: .add .play .next .current .queue .stats .clear .download")
+        return_output("Available commands: .add .play .next .current .queue .stats .download")
 
         try:
             args = command[5:]
@@ -69,7 +77,7 @@ def parse_command(command):
                 args = args[1:]
 
             if args == "add":
-                return_output(".add - adds tracks")
+                return_output(".add [search term] - queues tracks for playback")
             elif args == "play":
                 return_output(".play - starts playing (useful for when the stream dies due to lack of queued tracks")
             elif args == "next":
@@ -80,10 +88,8 @@ def parse_command(command):
                 return_output(".queue - shows next 4 songs")
             elif args == "stats":
                 return_output(".stats - shows stats")
-            elif args == "clear":
-                return_output(".clear - clears curent queue")
-            elif args == "download" or "dl":
-                return_output(".download | .dl - downloads track from URL and updates db")
+            elif args == "download" or args == "dl":
+                return_output("<.download | .dl> [url] - downloads track from URL and queues it")
         except:
             pass
 
@@ -117,8 +123,7 @@ while 1:
             if "*" in command:
                 return_output("NOPE")
             elif command.startswith("."):
-                print command
-                thread.start_new_thread(parse_command, command[1:])
+                thread.start_new_thread(parse_command, (command[1:], ""))
     except Exception, e:
         return_output(e)
         return_output("I dun goofed, soz aye m80.")
