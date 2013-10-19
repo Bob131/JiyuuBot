0a07dc0ad3b0c98ec98b231bd6e8eef0110e3595
Bob131 <bob@bob131.so>
2013-10-20 08:42:01 +1100

    Added dict mapping for help

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import mpd
 import ssl
 import os
 import urllib2
-import thread
+import threading
 import time
+import sys
 
 MPD_HOST = "127.0.0.1"
 MPD_PORT = 6600
@@ -16,6 +17,8 @@ PORT = 9999
 SSL = True
 
 MUSIC_PATH = "/media/music/Music"
+
+HELP_DICT = {"add": ".add [search term] - queues tracks for playback", "play": ".play - starts playing (useful for when the stream dies due to lack of queued tracks", "next": ".next - next track", "current": ".current - prints infor about current track", "queue": ".queue - shows next 4 songs", "stats": ".stats - shows stats", "download": "<.download | .dl> [url] - downloads track from URL and queues it", "dl": "<.download | .dl> [url] - downloads track from URL and queues it"}
 
 
 mpc = mpd.MPDClient()
@@ -35,18 +38,19 @@ if not os.path.exists(os.path.join(MUSIC_PATH, NICK+"_downloaded_music")):
 
 
 
-def parse_command(command, what):
+def reconnect_mpd():
     try:
-        mapped = command[:command.index(" ")]
-    except ValueError:
-        mapped = command
-    COMMAND_MAPPING[mapped](command)
+        mpc.disconnect()
+    except mpd.ConnectionError:
+        pass
+    mpc.connect(MPD_HOST, MPD_PORT)
+
 
 def cmd_play(command):
-    mpc.play()
+        mpc.play()
 
 def cmd_next(command):
-    mpc.next()
+        mpc.next()
 
 def cmd_download(command):
     try:
@@ -80,39 +84,26 @@ def cmd_add(command):
     mpc.searchadd("any", args)
 
 def cmd_current(command):
+    song_info = ""
     return_output(mpc.currentsong())
 
 def cmd_queue(command):
-    queue = "Next 4 tracks:\n"
-    for track in mpc.playlist()[ : 4]:
-        queue += str(track)+"\n"
-    return_output(queue)
+    queue = mpc.playlist()
+    queuestr = "Next 4 of %s tracks:\n" % len(queue)
+    for track in queue[ : 4]:
+        queuestr += str(track)+"\n"
+    return_output(queuestr)
 
 def cmd_stats(command):
     return_output(mpc.stats())
 
 def cmd_help(command):
     return_output("Available commands: .add .play .next .current .queue .stats .download")
-
     try:
         args = command[5:]
         if args.startswith("."):
             args = args[1:]
-
-        if args == "add":
-            return_output(".add [search term] - queues tracks for playback")
-        elif args == "play":
-            return_output(".play - starts playing (useful for when the stream dies due to lack of queued tracks")
-        elif args == "next":
-            return_output(".next - next track")
-        elif args == "current":
-            return_output(".current - prints infor about current track")
-        elif args == "queue":
-            return_output(".queue - shows next 4 songs")
-        elif args == "stats":
-            return_output(".stats - shows stats")
-        elif args == "download" or args == "dl":
-            return_output("<.download | .dl> [url] - downloads track from URL and queues it")
+        return_output(HELP_DICT[args])
     except:
         pass
 
@@ -123,6 +114,22 @@ def return_output(text):
     text=str(text)
     for msg in text.split("\n"):
         s.send("PRIVMSG " + HOME_CHANNEL + " :" + str(msg) + "\r\n")
+
+
+def parse_command(command):
+    try:
+        mapped = command[:command.index(" ")]
+    except ValueError:
+        mapped = command
+    try:
+        COMMAND_MAPPING[mapped](command)
+    except (KeyError, mpd.ConnectionError) as e:
+        if type(e) == mpd.ConnectionError:
+            reconnect_mpd()
+            COMMAND_MAPPING[mapped](command)
+        else:
+            pass
+
 
 
 COMMAND_MAPPING = {"dl": cmd_download, "download": cmd_download, "add": cmd_add, "play": cmd_play, "next": cmd_next, "current": cmd_current, "queue": cmd_queue, "stats": cmd_stats, "help": cmd_help}
@@ -138,14 +145,19 @@ while 1:
 	print line
 
 while 1:
-    line = s.recv(2048)
-    line = line.strip("\r\n")
-    print line
-    if "PING" in line:
-        s.send("PONG :" + line[6 : ])
-    elif "PRIVMSG" in line and not "NOTICE" in line and HOME_CHANNEL in line:
-        command = line[line.rindex(HOME_CHANNEL + " :") + len(HOME_CHANNEL) + 2 : ]
-        if "*" in command:
-            return_output("NOPE")
-        elif command.startswith("."):
-            thread.start_new_thread(parse_command, (command[1 : ], ""))
+    try:
+        line = s.recv(2048)
+        line = line.strip("\r\n")
+        print line
+        if "PING" in line:
+            s.send("PONG :" + line[6 : ])
+        elif "PRIVMSG" in line and not "NOTICE" in line and HOME_CHANNEL in line:
+            command = line[line.rindex(HOME_CHANNEL + " :") + len(HOME_CHANNEL) + 2 : ]
+            if "*" in command:
+                return_output("NOPE")
+            elif command.startswith("."):
+                t = threading.Thread(target = parse_command, args=(command[1 : ],))
+                t.daemon = 1
+                t.start()
+    except KeyboardInterrupt:
+        sys.exit()
