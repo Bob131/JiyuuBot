c401e8f53e08b19ee6efc2c5254efed3a1246ae9
Zaexu <zaexu@chokore-to.com>
2013-10-26 11:37:58 -0700

    Added parsing for better looking .current command

--- modules/current.py
+++ modules/current.py
@@ -1,5 +1,15 @@
 def current(self, lel):
-    self.conman.privmsg(self.conman.mpc.currentsong())
+    currentTrack = self.conman.mpc.currentsong()
+    if not currentTrack.get('time', "N/A") == "N/A":
+        m, s = divmod(int(currentTrack.get('time', "N/A")), 60)
+        time = "%d:%d" % (m, s)
+    else:
+        time = "N/A"
+    title = currentTrack.get('title',"N/A")
+    if title == "N/A":
+        title = os.path.basename(currentTrack.get("file", "N/A")).replace(" - ", " ")
+    parse = currentTrack.get('artist',"N/A") + " - " + currentTrack.get('album',"N/A") + " - " + title + " - " + time
+    self.conman.privmsg(parse)
 
 self.map_command("current", current)
 self.map_help("current", ".current - displays current track info")
