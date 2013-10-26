cdf6352467fb818de05aa73efcc1fd067baa8c2d
Bob131 <bob@bob131.so>
2013-10-27 02:34:40 +1100

    Improved randomisation

--- modules/add_random.py
+++ modules/add_random.py
@@ -1,16 +1,24 @@
 def add_random(self, command):
     import random
+    global selected_songs
     filelist = os.listdir(os.path.join(MUSIC_PATH, NICK + "_downloaded_music"))
     numsongs = int(self.confman.get_value("add_random", "NUMBER_OF_SONGS", 10))
     for i in range(1,numsongs):
-        filepath = filelist[random.randint(0, len(filelist)-1)]
-        filepath = os.path.join(NICK + "_downloaded_music", filepath)
-        # this is just a quick solution so that we didn't have so much fucking Dragonforce playing all the time
-        if not os.path.isdir(os.path.join(MUSIC_PATH, filepath)):
-            try:
-                self.conman.mpc.add(filepath)
-            except mpd.MPDError:
-                pass
+        if len(selected_songs) == len(filelist):
+            selected_songs = []
+        filepath = ""
+        while 1:
+            filepath = filelist[random.randint(0, len(filelist)-1)]
+            filepath = os.path.join(NICK + "_downloaded_music", filepath)
+            # this is just a quick solution so that we didn't have so much fucking Dragonforce playing all the time
+            if not os.path.isdir(os.path.join(MUSIC_PATH, filepath)) and not filepath in selected_songs:
+                break
+        selected_songs.append(filepath)
+        try:
+            self.conman.mpc.add(filepath)
+        except mpd.MPDError:
+            pass
 
+selected_songs = []
 self.map_command("random", add_random)
 self.map_help("random", ".random - adds 10 random tracks to the queue")
--- modules/announce.py
+++ modules/announce.py
@@ -1,10 +1,18 @@
 def announce(self, command):
     import random
+    global selected_intros
     filelist = os.listdir(os.path.join(MUSIC_PATH, NICK + "_intros"))
-    filepath = filelist[random.randint(0, len(filelist)-1)]
-    filepath = os.path.join(NICK + "_intros", filepath)
-    self.conman.privmsg("Adding %s" % filepath)
+    filepath = ""
+    while 1:
+        if len(selected_intros) == len(filelist):
+            selected_intros = []
+        filepath = filelist[random.randint(0, len(filelist)-1)]
+        filepath = os.path.join(NICK + "_intros", filepath)
+        if not filepath in selected_intros:
+            break
+    selected_intros.append(filepath)
     self.conman.mpc.addid(filepath, 1)
 
+selected_intros = []
 self.map_command("announce", announce)
 self.map_help("announce", ".announce - adds an intro track to the queue")
