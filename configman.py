1cbe8ce8b02845121f2c9a2e9d83fa9b083519a2
Bob131 <bob@bob131.so>
2013-12-26 00:25:22 +1100

    Changed load func

--- configman.py
+++ configman.py
@@ -3,13 +3,11 @@ import json
 
 class ConfigMan:
     def __init__(self, conftype):
-        if conftype == "module" or conftype == "service" or conftype == "perms":
-            self.configpath = os.path.join(os.path.dirname(__file__), "configs" + os.sep + conftype + ".json")
-        else:
-            raise Exception("Invalid config type")
-        self.values = self.load_values()
+        self.configpath = os.path.join(os.path.dirname(__file__), "configs" + os.sep + conftype + ".json")
+        self.load_values()
 
     def get_value(self, modname, valname, default="", writeDefault=True):
+        self.load_values()
         if self.values.has_key(modname) and self.values[modname].has_key(valname):
             return self.values[modname][valname]
         else:
@@ -28,9 +26,9 @@ class ConfigMan:
             config = open(self.configpath, "r")
             json_blocks = config.read()
             config.close()
-            return json.loads(json_blocks)
+            self.values = json.loads(json_blocks)
         except:
-            return {}
+            self.values = {}
 
     def save_values(self):
         config = open(self.configpath, "w")
