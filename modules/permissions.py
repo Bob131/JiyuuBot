22cd0616a415a3c6afe926407aaf9141bc9640fa
Bob131 <bob@bob131.so>
2013-12-27 07:51:18 +1100

    Filter out leading .s in perms module

--- modules/permissions.py
+++ modules/permissions.py
@@ -3,6 +3,8 @@ def get_perm_argparser(self, args):
     if args[0] == "nick":
         self.conman.privmsg("Permission level for %s: %s" % (args[1], self.permsman.get_nick_perms(args[1])))
     elif args[0] == "cmd":
+        if args[1].startswith("."):
+            args[1] = args[1][1:]
         self.conman.privmsg("Permission level for %s: %s" % (args[1], self.permsman.get_cmd_perms(args[1])))
 
 def set_perm_argparser(self, args):
@@ -11,6 +13,8 @@ def set_perm_argparser(self, args):
         self.conman.privmsg("Setting permission level for %s: %s" % (args[1], args[2]))
         self.permsman.set_nick_perms(args[1], args[2])
     elif args[0] == "cmd":
+        if args[1].startswith("."):
+            args[1] = args[1][1:]
         self.conman.privmsg("Setting permission level for %s: %s" % (args[1], args[2]))
         self.permsman.set_cmd_perms(args[1], args[2])
 
