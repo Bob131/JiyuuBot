36239957027c1c91c3231c2d25c6dcb3164f27db
Bob131 <bob@bob131.so>
2013-12-31 03:15:56 +1100

    Fix queue module

--- modules/queue.py
+++ modules/queue.py
@@ -4,6 +4,7 @@ def queue(self, command):
     queue = self.conman.mpc.playlist()
     queuestr = "Next 4 of %s tracks:\n" % len(queue)
     for track in queue[ : 4]:
+        track = track.replace("file: ", "")
         queuestr += self.funcs["format_song_details"](self, track)+"\n"
     self.conman.privmsg(queuestr)
 
