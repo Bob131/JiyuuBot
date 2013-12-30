857ffb0299c13e47c106d36ddc99e596c6740ac9
Bob131 <bob@bob131.so>
2013-12-30 21:42:53 +1100

    Added string formatting to stats.py

--- modules/stats.py
+++ modules/stats.py
@@ -1,6 +1,9 @@
 #Define function to display stats of MPCD
 def stats(self, cmd):
-    self.conman.privmsg(self.conman.mpc.stats())
+    import datetime
+    statsdict = self.conman.mpc.stats()
+    self.conman.privmsg("Uptime: %s - Playtime: %s" % (datetime.timedelta(seconds=int(statsdict["uptime"])), datetime.timedelta(seconds=int(statsdict["playtime"]))))
+    self.conman.privmsg("Artists: %s - Albums: %s - Songs: %s" % (statsdict["artists"], statsdict["albums"], statsdict["songs"]))
 
 #Maps command and help text for command
 self.map_command("stats", stats)
