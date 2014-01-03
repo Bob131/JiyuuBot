5e2846796b57b4f032aee62a2d1e3e79196aea1e
Bob131 <bob@bob131.so>
2014-01-04 06:45:05 +1100

    Fixed pull.py

--- modules/pull.py
+++ modules/pull.py
@@ -1,6 +1,7 @@
 def pull(self, command):
     import subprocess
     output = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-    self.conman.privmsg(output.stdout)
+    for line in output.stdout:
+        self.conman.privmsg("***Git output: %s" % line)
 
 self._map("command", "pull", pull)
