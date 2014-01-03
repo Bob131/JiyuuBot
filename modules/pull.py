c08bdb6a292267d7e8a7e7e8666facc9fb3c9b10
Bob131 <bob@bob131.so>
2014-01-04 06:50:56 +1100

    Added stderr output to pull.py

--- modules/pull.py
+++ modules/pull.py
@@ -1,7 +1,9 @@
 def pull(self, command):
     import subprocess
-    output = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+    output = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     for line in output.stdout:
         self.conman.privmsg("***Git output: %s" % line)
+    for line in output.stderr:
+        self.conman.privmsg("***Git error: %s" % line)
 
 self._map("command", "pull", pull)
