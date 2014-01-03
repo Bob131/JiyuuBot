193c5b45dd77cc5f2dcfcbb3f8cff181807c90c7
Bob131 <bob@bob131.so>
2014-01-04 06:39:45 +1100

    Added mapping to pull.py

--- modules/pull.py
+++ modules/pull.py
@@ -2,3 +2,5 @@ def pull(self, command):
     import subprocess
     output = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
     self.conman.privmsg(output.stdout)
+
+self._map("command", "pull", pull)
