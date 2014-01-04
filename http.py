15d02952c93782ddd865ad181c5c3d8be16022a1
Bob131 <bob@bob131.so>
2014-01-05 07:58:32 +1100

    Fix timeout

--- http.py
+++ http.py
@@ -12,17 +12,25 @@ class httpd_api:
         http_responses = httpreps
         self.plugman = plugman_instance
         self.httpd = wsgiref.simple_server.make_server(HTTPD_IP, HTTPD_PORT, self.http_parse)
-        t = threading.Thread(target = self.httpd.serve_forever)
+        t = threading.Thread(target = self.httpd_serve)
         t.daemon = True
         t.start()
 
+    def httpd_serve(self):
+        while 1:
+            try:
+                self.httpd.serve_forever()
+            except Exception as e:
+                print str(e)
+
     def http_parse(self, environ, start_response):
-        path = environ["PATH_INFO"][1:].replace("/", "")
+        path = environ["PATH_INFO"][1:]
         path = urllib.unquote_plus(path)
+        command = path.split(" ")[0]
         toreturn = ""
         if path == "robots.txt":
             toreturn = "User-agent: *\nDisallow: /"
-        else:
+        elif command in self.plugman.httplist.keys():
             tid = self.plugman.execute_command(path, "HTTP")
             while 1:
                 if tid in http_responses.keys():
@@ -30,5 +38,7 @@ class httpd_api:
                 time.sleep(1)
             toreturn = http_responses[tid]
             del http_responses[tid]
+        else:
+            toreturn = "{ \"Invalid command\" }"
         start_response("200 OK", [("Content-type", "text/plain")])
         return [toreturn]
