bb78bd821142cc5970b9bf7431787fe600af8c02
Bob131 <bob@bob131.so>
2014-01-06 01:26:07 +1100

    Fixed handler threads dying after one request

--- http.py
+++ http.py
@@ -20,34 +20,35 @@ class httpd_api:
             t.start()
 
     def http_parse(self, sock):
-        client, addr = sock.accept()
-        req = client.recv(2048)
-        path = ""
-        for line in req.split("\n"):
-            if line.startswith("GET"):
-                print "HTTPD: '%s' from %s at %s" % (line, addr, time.strftime("%Y-%m-%d %H:%M:%S"))
-                path = line.split(" ")[1]
-                break
-        path = urllib.unquote_plus(path[1:])
-        command = path.split(" ")[0]
-        resp_code = "200 OK"
-        toreturn = ""
-        if path == "robots.txt":
-            toreturn = "User-agent: *\nDisallow: /"
-        elif command in self.plugman.httplist.keys():
-            tid = self.plugman.execute_command(path, "HTTP")
-            for x in range(0,10):
-                if tid in http_responses.keys():
+        while 1:
+            client, addr = sock.accept()
+            req = client.recv(2048)
+            path = ""
+            for line in req.split("\n"):
+                if line.startswith("GET"):
+                    print "HTTPD: '%s' from %s at %s" % (line, addr, time.strftime("%Y-%m-%d %H:%M:%S"))
+                    path = line.split(" ")[1]
                     break
-                time.sleep(1)
-            try:
-                toreturn = http_responses[tid]
-                del http_responses[tid]
-            except KeyError:
-                toreturn = "\"No output\""
-        else:
-            toreturn = "\"Invalid command\""
-            resp_code = "404 Not Found"
-        toreturn = "HTTP/1.1 %s\nServer: Jiyuu Radio\nContent-Type: text/plain\n\n%s" % (resp_code, toreturn)
-        client.send(toreturn)
-        client.close()
+            path = urllib.unquote_plus(path[1:])
+            command = path.split(" ")[0]
+            resp_code = "200 OK"
+            toreturn = ""
+            if path == "robots.txt":
+                toreturn = "User-agent: *\nDisallow: /"
+            elif command in self.plugman.httplist.keys():
+                tid = self.plugman.execute_command(path, "HTTP")
+                for x in range(0,10):
+                    if tid in http_responses.keys():
+                        break
+                    time.sleep(1)
+                try:
+                    toreturn = http_responses[tid]
+                    del http_responses[tid]
+                except KeyError:
+                    toreturn = "\"No output\""
+            else:
+                toreturn = "\"Invalid command\""
+                resp_code = "404 Not Found"
+            toreturn = "HTTP/1.1 %s\nServer: Jiyuu Radio\nContent-Type: text/plain\n\n%s" % (resp_code, toreturn)
+            client.send(toreturn)
+            client.close()
