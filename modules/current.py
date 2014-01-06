b7316757374e9f44657f07e6ffe85bad69a6d55a
Bob131 <bob@bob131.so>
2014-01-07 06:10:58 +1100

    Added elapsed value to JSON output of current module

--- modules/current.py
+++ modules/current.py
@@ -1,8 +1,12 @@
 def current(self, lel):
     self.require("format_song_details")
     currentTrack = self.conman.mpc.currentsong()
+    if type(currentTrack) == list:
+        currentTrack = currentTrack[0]
     tosend = self.run_func("format_song_details", [currentTrack["file"]])
     if thread_types[threading.current_thread().ident] == "HTTP":
+        import math
+        currentTrack["elapsed"] = math.floor(float(self.conman.mpc.status()["elapsed"]))
         tosend = json.dumps(currentTrack, sort_keys=True, indent=4, separators=(',', ': '))
     self.conman.gen_send(tosend)
 
