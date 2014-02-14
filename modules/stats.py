b334dcece96cde6cc8ab1f0b238ffde302b6d0ad
Bob131 <bob@bob131.so>
2014-02-14 15:09:47 +1100

    stats.py now uses gen_send()

--- modules/stats.py
+++ modules/stats.py
@@ -5,8 +5,8 @@ def stats(self, cmd):
         self.conman.gen_send(json.dumps(statsdict, sort_keys=True, indent=4, separators=(',', ': ')))
     else:
         import datetime
-        self.conman.privmsg("Uptime: %s - Playtime: %s" % (datetime.timedelta(seconds=int(statsdict["uptime"])), datetime.timedelta(seconds=int(statsdict["playtime"]))))
-        self.conman.privmsg("Artists: %s - Albums: %s - Songs: %s" % (statsdict["artists"], statsdict["albums"], statsdict["songs"]))
+        self.conman.gen_send("Uptime: %s - Playtime: %s" % (datetime.timedelta(seconds=int(statsdict["uptime"])), datetime.timedelta(seconds=int(statsdict["playtime"]))))
+        self.conman.gen_send("Artists: %s - Albums: %s - Songs: %s" % (statsdict["artists"], statsdict["albums"], statsdict["songs"]))
 
 #Maps command and help text for command
 self._map("command", "stats", stats)
