e01e003c9bdd6d0e828d662cdac6ca7c296773c7
Bob131 <bob@bob131.so>
2014-02-19 01:55:54 +1100

    Added wiki module

---
+++ modules/wiki.py
@@ -0,0 +1,13 @@
+def wikipedia(self, string):
+    import requests
+    UA = "JiyuuBot/1 (http://github.com/JiyuuProject/JiyuuBot; bob@bob131.so) BasedOnRequests/%s" % requests.__version__
+    matches = re.findall("(\w+).wikipedia.org/wiki/(\w+)", string)
+    for match in matches:
+        categories = requests.get("http://%s.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=10&cldir=descending&titles=%s&redirects=" % match, headers={"user-agent": UA}).json()["query"]["pages"]
+        categories = categories[categories.keys()[0]]
+        if "missing" in categories.keys():
+            self.conman.gen_send("Page not found")
+        else:
+            self.conman.gen_send("%s | Categories: %s" % (categories["title"], ", ".join(x["title"].replace("Category:", "") for x in categories["categories"])))
+
+self._map("regex", ".*\w+.wikipedia.org/wiki/\w+.*", wikipedia, "Wikipedia")
