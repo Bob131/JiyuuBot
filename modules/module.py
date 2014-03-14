20fefc68d2e720242bf02d5bea28b28ab4aae565
Bob131 <bob@bob131.so>
2014-03-15 01:02:46 +1100

    youtube stuff

--- modules/module.py
+++ modules/module.py
@@ -1,14 +1,15 @@
 def mod_parse(self, cmd):
     arg = cmd.split(" ")
+    print(arg)
     if arg[0] == "list":
-        self.conman.gen_send(", ".join(plugin.replace(self.modulespath, "").replace(".py", "") for plugin in self.pluginlist if arg[1] in self.pluginlist[plugin]))
-    if arg[0] == "block":
+        self.conman.gen_send(", ".join(plugin.replace(self.modulespath, "").replace(".py", "") for plugin in self.pluginlist))
+    elif arg[0] == "block":
         block = set(self.glob_confman.get_value("modules", "BLOCKLIST", []))
         for mod in arg[1].split(","):
             block.add(mod)
         self.glob_confman.set_value("modules", "BLOCKLIST", list(block))
         self.conman.gen_send("Blocked %s modules" % len(arg[1].split(",")))
-    if arg[0] == "unblock":
+    elif arg[0] == "unblock":
         block = set(self.glob_confman.get_value("modules", "BLOCKLIST", []))
         for mod in arg[1].split(","):
             block.remove(mod)
--- modules/youtube.py
+++ modules/youtube.py
@@ -5,12 +5,14 @@ def youtube(self, string):
         for match in stuff:
             if "v=" in match:
                 match = re.findall("v=([\w-]+[^&\s])", match)[0]
-            jdata = requests.get("https://gdata.youtube.com/feeds/api/videos/%s?alt=jsonc&v=2" % match).json()
+            jdata = requests.get("https://gdata.youtube.com/feeds/api/videos/%s?alt=json&v=2" % match).json()
             try:
                 import datetime
-                jdata = jdata["data"]
-                duration = str(datetime.timedelta(seconds=jdata["duration"]))
-                self.conman.gen_send("\x02%s\x02 - Uploaded by \x02%s\x02 - Uploaded %s - %s views - Duration %s" % (jdata["title"], jdata["uploader"], jdata["uploaded"].split("T")[0], jdata["viewCount"], duration))
+                import locale
+                jdata = jdata["entry"]
+                duration = str(datetime.timedelta(seconds=int(jdata["media$group"]["yt$duration"]["seconds"])))
+                locale.setlocale(locale.LC_ALL, "en_US")
+                self.conman.gen_send("\x02%s\x02 - Uploaded by \x02%s\x02 - Uploaded %s - %s views - Duration %s" % (jdata["title"]["$t"], jdata["author"][0]["name"]["$t"], jdata["published"]["$t"].split("T")[0], locale.format("%d", int(jdata["yt$statistics"]["viewCount"]), grouping=True), duration))
             except KeyError:
                 self.conman.gen_send("%s" % jdata["error"]["message"])
 
