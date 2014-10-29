1ca81795ca20286277402f3a68f13394cc6de11c
Bob131 <bob@bob131.so>
2014-10-30 03:22:41 +1100

    Remove depricated modules

--- modules/file.py
+++
@@ -1,7 +0,0 @@
-def rfile(self, asdasd):
-    tosend = self.conman.mpc.currentsong()
-    if type(tosend) == list:
-        tosend = tosend[0]
-    self.conman.gen_send("\"%s\"" % tosend["file"])
-
-self._map("http", "file", rfile)
