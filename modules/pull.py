ccbea8cd37acdfa7e9962d28ad736942fa857b26
Bob131 <bob@bob131.so>
2014-10-30 15:08:36 +1100

    Fix pull.py

--- modules/pull.py
+++ modules/pull.py
@@ -2,9 +2,9 @@ def pull(self, msginfo):
     import subprocess
     output = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     for line in output.stdout:
-        self.conman.gen_send("***Git output: %s" % line.decode("UTF-8"), msginfo)
+        self.conman.gen_send("***Git output: %s" % line.decode("UTF-8").replace("\n", ""), msginfo)
     for line in output.stderr:
-        self.conman.gen_send("***Git error: %s" % line.decode("UTF-8"), msginfo)
+        self.conman.gen_send("***Git error: %s" % line.decode("UTF-8").replace("\n", ""), msginfo) # replace required to not trigger the privmsg() except
 
 self.commandlist["pull"] = {
         "type": MAPTYPE_COMMAND,
