abdc0aad80a58606aff0d5642c6184717fa46412
Bob131 <bob@bob131.so>
2014-10-31 19:04:31 +1100

    Feature add

--- modules/git.py
+++ modules/git.py
@@ -11,7 +11,8 @@ def git_get_name(self, userdict, from_osrc=False):
 
     # if dict from api.github.com
     if "name" in userdict.keys():
-        if userdict["name"] == None or userdict["name"] == userdict["login"]:
+        print(userdict["name"])
+        if userdict["name"] == None or userdict["name"].strip() == "" or userdict["name"] == userdict["login"]:
             return userdict["login"]
         else:
             return strfrmt % (userdict["name"], userdict["login"])
@@ -64,7 +65,7 @@ def git(self, msginfo):
             # if issue number provided
             if len(match) == 4:
                 req = requests.get("https://api.github.com/repos/%s/%s/%s/%s" % tuple(match)).json()
-                self.conman.gen_send("%s/%s#%s - \x02%s\x02 - by \x02%s\x02 - Created: %s - Updated: %s" % (match[0], match[1], req["number"], req["title"], self.funcs["git_get_name"](self, req["user"]), req["created_at"].split("T")[0], req["updated_at"].split("T")[0]), msginfo)
+                self.conman.gen_send("%s/%s#%s - \x02%s\x02 - by \x02%s\x02 - Created: %s - Updated: %s - State: %s - Tags: %s" % (match[0], match[1], req["number"], req["title"], self.funcs["git_get_name"](self, req["user"]), req["created_at"].split("T")[0], req["updated_at"].split("T")[0], req["state"].capitalize(), ", ".join("\x02%s\x02" % label["name"] for label in req["labels"])), msginfo)
 
         # if github link to file
         elif match[2] == "tree":
