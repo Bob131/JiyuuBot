3f6931d1288667da0b42b52e57673227c19835d4
Bob131 <bob@bob131.so>
2014-11-28 18:55:34 +1100

    Fix char encoding

--- modules/nyaa.py
+++ modules/nyaa.py
@@ -5,13 +5,16 @@ def nyaa(self, msginfo):
     for url in urls:
         request = requests.get("http://nyaa.se/?page=view&tid={}".format(url))
         if request.status_code == 200 and not "does not appear to be in the database" in request.text:
-            soup = BeautifulSoup(request.text)
+            print(request.encoding)
+            soup = BeautifulSoup(request.text.encode(request.encoding))
+            print(soup.original_encoding)
             name = soup.find(class_="viewtorrentname").contents[0]
+            print(name)
             size = soup.find_all(class_="vtop")[-1].contents[0]
             # seeders/leachers
             up = soup.find(class_="viewsn").contents[0]
             down = soup.find(class_="viewln").contents[0]
-            tosend = "\x02{}\x02 - Size: {} - Peers: {} - Download: {}".format(name, size, "\x033↑{}/\x034↓{}\x03".format(up, down), "http://nyaa.se/view=download&tid={}".format(url))
+            tosend = "\x02{}\x02 - Size: {} - Peers: {} - Download: {}".format(name, size, "\x033↑{}/\x034↓{}\x03".format(up, down), "http://nyaa.se/?view=download&tid={}".format(url))
             self.conman.gen_send(tosend, msginfo)
         elif request.status_code == 200:
             self.conman.gen_send("Error: Torrent ID \x02{}\x02 not found".format(url), msginfo)
