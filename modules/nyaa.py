a99ee3bfa9652579d7692db5a67fac6d5de87c24
Bob131 <bob@bob131.so>
2014-11-28 19:12:36 +1100

    Fix for sukebei

--- modules/nyaa.py
+++ modules/nyaa.py
@@ -1,9 +1,10 @@
 def nyaa(self, msginfo):
     import requests
     from bs4 import BeautifulSoup
-    urls = re.findall("nyaa.se/\\?page=view&tid=(\d+)", msginfo["msg"])
+    urls = re.findall("https?://([\w\.]*)nyaa.se/\\?page=view&tid=(\d+)", msginfo["msg"])
     for url in urls:
-        request = requests.get("http://nyaa.se/?page=view&tid={}".format(url))
+        lewd, url = url
+        request = requests.get("http://{}nyaa.se/?page=view&tid={}".format(lewd, url))
         if request.status_code == 200 and not "does not appear to be in the database" in request.text:
             soup = BeautifulSoup(request.text.encode(request.encoding))
             name = soup.find(class_="viewtorrentname").contents[0]
@@ -11,7 +12,7 @@ def nyaa(self, msginfo):
             # seeders/leachers
             up = soup.find(class_="viewsn").contents[0]
             down = soup.find(class_="viewln").contents[0]
-            tosend = "\x02{}\x02 - Size: {} - Peers: {} - Download: {}".format(name, size, "\x033↑{}/\x034↓{}\x03".format(up, down), "http://nyaa.se/?view=download&tid={}".format(url))
+            tosend = "\x02{}\x02 - Size: {} - Peers: {} - Download: {}".format(name, size, "\x033↑{}/\x034↓{}\x03".format(up, down), "http://{}nyaa.se/?view=download&tid={}".format(lewd, url))
             self.conman.gen_send(tosend, msginfo)
         elif request.status_code == 200:
             self.conman.gen_send("Error: Torrent ID \x02{}\x02 not found".format(url), msginfo)
