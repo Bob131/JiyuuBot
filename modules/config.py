254da66cf583554c0705c429192e9e859642fa66
Bob131 <bob@bob131.so>
2014-11-29 18:22:24 +1100

    Actually init new module

--- modules/config.py
+++ modules/config.py
@@ -2,3 +2,9 @@ def configs(self, msginfo):
     # to be finished
     if msginfo["msg"].startswith(".config reload"):
         self.confman.load()
+
+
+self.commandlist["config"] = {
+        "type": MAPTYPE_COMMAND,
+        "function": configs
+        }
