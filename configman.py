f3ef76a5a9836a91636e477c4bcf18bc3388eb12
Bob131 <bob@bob131.so>
2014-12-04 00:09:38 +1100

    Fix race condition

--- configman.py
+++ configman.py
@@ -1,13 +1,15 @@
 import os
 import json
+import threading
 
 
 class ConfigMan:
     def __init__(self, conftype):
         self.configpath = os.path.join(os.path.dirname(__file__), "configs" + os.sep + conftype + ".json")
+        self.lock = threading.Lock()
+        self.load(True)
         # if global.json, ensure the required values are present
         if conftype == "global":
-            self.load()
             try:
                 _ = self.values["IRC"]["HOST"]
                 _ = self.values["IRC"]["HOME_CHANNEL"]
@@ -19,7 +21,6 @@ class ConfigMan:
                     _ = self.values["MPD"]["PORT"]
             except KeyError:
                 raise Exception("configs/global.json missing values. See configs/global.json.example for help")
-        self.load(True)
 
 
     def get(self, modname, valname, default="", writeDefault=True, allowTemp=True):
@@ -36,17 +37,22 @@ class ConfigMan:
 
     def setv(self, modname, valname, value, temp=False):
         if temp:
+            self.lock.acquire()
             if modname not in self.tvalues:
                 self.tvalues[modname] = {}
             self.tvalues[modname][valname] = value
+            self.lock.release()
         else:
+            self.lock.acquire()
             if modname not in self.values:
                 self.values[modname] = {}
             self.values[modname][valname] = value
+            self.lock.release()
             self.save()
 
 
     def load(self, setTempVals=False):
+        self.lock.acquire()
         try:
             config = open(self.configpath, "r")
             json_blocks = config.read()
@@ -56,10 +62,13 @@ class ConfigMan:
             self.values = {}
         if setTempVals:
             self.tvalues = {}
+        self.lock.release()
 
 
     def save(self):
+        self.lock.acquire()
         config = open(self.configpath, "w")
         config.write(json.dumps(self.values, sort_keys=True, indent=4, separators=(',', ': ')))
         config.flush()
         config.close()
+        self.lock.release()
