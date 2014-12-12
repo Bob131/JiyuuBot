72232a6e64d6476431042a84233401b87da9d7e7
Bob131 <bob@bob131.so>
2014-12-12 15:01:44 +1100

    Fix ping timeout recovery

--- main.py
+++ main.py
@@ -42,86 +42,87 @@ while 1:
         # receive and format line for parsing
         line = conman.s.recv()
 
-        if line.startswith(":"):
-            line = line[1:]
-
-        # respond to pings
-        if "PING" in line and not "PRIVMSG" in line:
-            # line[6:] strips out "PING: "
-            conman.queue_raw("PONG :" + line[6:])
-        elif "PRIVMSG" in line and "PING" in line:           
-            # what? I really have no idea whats going on here, but it seems to work
-            msg = "".join(line.split()[3:])[1:]   
-            sender = line.split(":")[1].split('!')[0]
-            conman.queue_raw("NOTICE %s :%s" % (sender, msg))
-
-        # we have a message! Parse it
-        elif "PRIVMSG" in line and not "NOTICE" in line and not "PING" in line:
-            # create struct for passing message info
-            # include original message + info parsed from it and other contextual data
-            # may also include values 'type' and 'pattern'; see below
-            msginfo = {
-                    "strg": line,
-                    "msg": "", # parsing requires values in this struct
-                    "chan": line[line.index(" PRIVMSG ") + 9 : line.index(" :")],
-                    "nick": line[:line.index("!")],
-                    "user": line[line.index("!")+1:line.index("@")].replace("~", ""),
-                    "hostname": line[line.index("@")+1:line.index(" ")],
-                    "timestamp": time.time()}
-            # strip out non-printable chars. See http://www.unicode.org/reports/tr44/tr44-6.html#Code_Point_Labels
-            for char in line[line.index(msginfo["chan"] + " :")+len(msginfo["chan"])+2:]:
-                if not unicodedata.category(char).startswith("C"):
-                    msginfo["msg"] += char
-            msginfo["msg"] = msginfo["msg"].strip()
-
-            if msginfo["msg"].lower().startswith(confman.get("IRC", "NICK").lower()):
-                msginfo["msg"] = msginfo["msg"][len(confman.get("IRC", "NICK")):]
-                msginfo["msg"] = re.sub("[,:\s]*", "", msginfo["msg"])
-
-            # if this is an IM, set chan to the offending nick so the response can be properly directed
-            if msginfo["chan"] == confman.get("IRC", "NICK"):
-                msginfo["chan"] = msginfo["nick"]
-                # if not authorized, notify the user
-                if not permsman.get_msg_perms(msginfo["hostname"]):
-                    conman.gen_send("You do not have adequate permissions for IM", msginfo)
-                    continue
-
-            # if msg is a command, execute
-            if msginfo["msg"].startswith("."):
-                # check whether its an actual command
-                if msginfo["msg"].split(" ")[0].replace(".", "") in plugman.commandlist.keys():
-                    # ensure adequate permssions (move this to load.py to enabled perms checking on regex functions?)
-                    if permsman.get_perms(msginfo["hostname"], msginfo["msg"].split(" ")[0][1:]):
-                        msginfo["type"] = "PRIVMSG"
-                        plugman.execute_command(msginfo)
-                    else:
-                        conman.gen_send("You do not have high enough permissions to execute that command", msginfo)
-
-            # check whether message matches any mapped regex strings
-            matches = list(pattern for pattern in plugman.regex_cache if not re.match(pattern, msginfo["msg"]) == None)
-            if len(matches) > 0:
-                msginfo["type"] = "regex"
-                for match in matches:
-                    msginfo["pattern"] = match
-                    plugman.execute_command(msginfo)
-
-        # irc connection lost; time to reconnect
-        elif line == "":
-            conman.reconnect_irc()
-
-        elif line == None:
+        if line == None:
             print("*** Ping timeout. Waiting 60 seconds and trying again ***")
             time.sleep(60)
             conman.reconnect_irc()
 
-        # allow modules to hook for other commands
         else:
-            command = line.split(" ")[1]
-            if "!" + command in plugman.commandlist.keys():
-                # offload all parsing to module
-                msginfo = {"strg": line, "type": "regex", "pattern": "!" + command} # reg as regex for simplicity
-                msginfo["chan"] = confman.get("IRC", "HOME_CHANNEL") # so error handlers know where to spit msgs
-                plugman.execute_command(msginfo)
+            if line.startswith(":"):
+                line = line[1:]
+
+            # respond to pings
+            if "PING" in line and not "PRIVMSG" in line:
+                # line[6:] strips out "PING: "
+                conman.queue_raw("PONG :" + line[6:])
+            elif "PRIVMSG" in line and "PING" in line:           
+                # what? I really have no idea whats going on here, but it seems to work
+                msg = "".join(line.split()[3:])[1:]   
+                sender = line.split(":")[1].split('!')[0]
+                conman.queue_raw("NOTICE %s :%s" % (sender, msg))
+
+            # we have a message! Parse it
+            elif "PRIVMSG" in line and not "NOTICE" in line and not "PING" in line:
+                # create struct for passing message info
+                # include original message + info parsed from it and other contextual data
+                # may also include values 'type' and 'pattern'; see below
+                msginfo = {
+                        "strg": line,
+                        "msg": "", # parsing requires values in this struct
+                        "chan": line[line.index(" PRIVMSG ") + 9 : line.index(" :")],
+                        "nick": line[:line.index("!")],
+                        "user": line[line.index("!")+1:line.index("@")].replace("~", ""),
+                        "hostname": line[line.index("@")+1:line.index(" ")],
+                        "timestamp": time.time()}
+                # strip out non-printable chars. See http://www.unicode.org/reports/tr44/tr44-6.html#Code_Point_Labels
+                for char in line[line.index(msginfo["chan"] + " :")+len(msginfo["chan"])+2:]:
+                    if not unicodedata.category(char).startswith("C"):
+                        msginfo["msg"] += char
+                msginfo["msg"] = msginfo["msg"].strip()
+
+                if msginfo["msg"].lower().startswith(confman.get("IRC", "NICK").lower()):
+                    msginfo["msg"] = msginfo["msg"][len(confman.get("IRC", "NICK")):]
+                    msginfo["msg"] = re.sub("[,:\s]*", "", msginfo["msg"])
+
+                # if this is an IM, set chan to the offending nick so the response can be properly directed
+                if msginfo["chan"] == confman.get("IRC", "NICK"):
+                    msginfo["chan"] = msginfo["nick"]
+                    # if not authorized, notify the user
+                    if not permsman.get_msg_perms(msginfo["hostname"]):
+                        conman.gen_send("You do not have adequate permissions for IM", msginfo)
+                        continue
+
+                # if msg is a command, execute
+                if msginfo["msg"].startswith("."):
+                    # check whether its an actual command
+                    if msginfo["msg"].split(" ")[0].replace(".", "") in plugman.commandlist.keys():
+                        # ensure adequate permssions (move this to load.py to enabled perms checking on regex functions?)
+                        if permsman.get_perms(msginfo["hostname"], msginfo["msg"].split(" ")[0][1:]):
+                            msginfo["type"] = "PRIVMSG"
+                            plugman.execute_command(msginfo)
+                        else:
+                            conman.gen_send("You do not have high enough permissions to execute that command", msginfo)
+
+                # check whether message matches any mapped regex strings
+                matches = list(pattern for pattern in plugman.regex_cache if not re.match(pattern, msginfo["msg"]) == None)
+                if len(matches) > 0:
+                    msginfo["type"] = "regex"
+                    for match in matches:
+                        msginfo["pattern"] = match
+                        plugman.execute_command(msginfo)
+
+            # irc connection lost; time to reconnect
+            elif line == "":
+                conman.reconnect_irc()
+
+            # allow modules to hook for other commands
+            else:
+                command = line.split(" ")[1]
+                if "!" + command in plugman.commandlist.keys():
+                    # offload all parsing to module
+                    msginfo = {"strg": line, "type": "regex", "pattern": "!" + command} # reg as regex for simplicity
+                    msginfo["chan"] = confman.get("IRC", "HOME_CHANNEL") # so error handlers know where to spit msgs
+                    plugman.execute_command(msginfo)
     except Exception as e:
         if not type(e) == InterruptedError:
             traceback.print_exc()
