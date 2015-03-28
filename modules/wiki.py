78e217db3c8d17eed4ba9fdd2a131cb8237751f4
Bob131 <bob@bob131.so>
2015-03-28 13:35:42 +1100

    Discard 'File:' wiki links

--- modules/wiki.py
+++ modules/wiki.py
@@ -6,9 +6,10 @@ def wikipedia(self, msginfo):
     matches = re.findall("(\w+).wikipedia.org/wiki/([^\#\s\?]+)", msginfo["msg"])
     for match in matches:
         match = (match[0], parse.unquote(match[1]))
-        categories = requests.get("http://%s.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=10&cldir=descending&titles=%s&redirects=" % match, headers={"user-agent": UA}).json()["query"]["pages"]
-        categories = categories[list(categories.keys())[0]]
-        if "missing" in categories.keys():
-            self.conman.gen_send("Page not found", msginfo)
-        else:
-            self.conman.gen_send("%s | Categories: %s" % (categories["title"], ", ".join(re.sub("\w+:", "", x["title"]) for x in categories["categories"])), msginfo)
+        if not match[1].startswith("File:"):
+            categories = requests.get("http://%s.wikipedia.org/w/api.php?action=query&prop=categories&format=json&cllimit=10&cldir=descending&titles=%s&redirects=" % match, headers={"user-agent": UA}).json()["query"]["pages"]
+            categories = categories[list(categories.keys())[0]]
+            if "missing" in categories.keys():
+                self.conman.gen_send("Page not found", msginfo)
+            else:
+                self.conman.gen_send("%s | Categories: %s" % (categories["title"], ", ".join(re.sub("\w+:", "", x["title"]) for x in categories["categories"])), msginfo)
