d1fe884e875a89b1407f946734c78ba5ec17b9e6
Ken Swenson <zaexu@chokore-to.com>
2015-05-25 14:24:03 -0400

    Fix youtu.be regex; closes #33

--- modules/youtube.py
+++ modules/youtube.py
@@ -1,18 +1,21 @@
-@self.regex(".*(youtube\.com|youtu\.be)/(watch\?)?([\w\d]+).*", prefix="YouTube")
+@self.regex(".*(youtube\.com|youtu\.be)/(watch\?)?([\w\d-]+).*", prefix="YouTube")
 def youtube(self, msginfo):
     string = msginfo["msg"]
-    stuff = re.findall("youtu.be/([\w\d]+)", string) + re.findall("youtube.com/watch\?v=([\w\d]+)", string)
+    stuff = re.findall("youtu.be/([\w-]+[^&\s])", string) + re.findall("youtube.com/watch\?v=([\w\d-]+)", string)
     if len(stuff) > 0:
         import requests
         for match in stuff:
             if "v=" in match:
                 match = re.findall("v=([\w-]+[^&\s])", match)[0]
+            if ".be\/" in match:
+                match = re.findall(".be\/([\w-]+[^&\s])")[0]
             try:
                 if not self.confman.get("youtube", "apikey") == "":
                     auth = self.confman.get("youtube", "apikey", "")
                 else:
                     auth = None
                 jdata = requests.get("https://www.googleapis.com/youtube/v3/videos?id=%s&key=%s&part=snippet,contentDetails,statistics,status" % (match, auth)).json()
+                print(match)
                 import datetime
                 import locale
                 jdata = jdata["items"][0]
