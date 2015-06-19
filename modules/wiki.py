0fe9cad82bdcadfe7f97caf1efc9aa570b36b41b
Philipp Schur <philipp@jiyuu.de>
2015-06-20 00:10:32 +0200

    Added wiki search; closes #34

--- modules/wiki.py
+++ modules/wiki.py
@@ -1,17 +1,32 @@
 import re
-from urllib import parse
+import urllib.parse
 import requests
-from . import regex_handler, send
+from . import functions, command, regex_handler, send
+
+UA = "JiyuuBot/1 (http://github.com/Bob131/JiyuuBot; bob@bob131.so) BasedOnRequests/{}".format(requests.__version__)
+
+@functions
+def get_wiki_summary(title, lang='en'):
+    summaries = requests.get("http://{}.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&format=json&cllimit=10&cldir=descending&titles={}&redirects=".format(lang, title), headers={"user-agent": UA}).json()["query"]["pages"]
+    summaries = summaries[list(summaries.keys())[0]]
+    if not "missing" in summaries.keys():
+        return summaries["title"], re.split("(?<=[a-z]\.)\s+", summaries["extract"])[0]
+
 
 @regex_handler(".*\w+.wikipedia.org/wiki/[^\#\s\?>]+.*")
 def wikipedia(msginfo):
-    UA = "JiyuuBot/1 (http://github.com/JiyuuProject/JiyuuBot; bob@bob131.so) BasedOnRequests/%s" % requests.__version__
     matches = re.findall("(\w+).wikipedia.org/wiki/([^\#\s\?]+)", msginfo["msg"])
     for match in matches:
-        match = (match[0], parse.unquote(match[1]))
+        match = (match[0], urllib.parse.unquote(match[1]))
         if not match[1].startswith("File:"):
-            summaries = requests.get("http://%s.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&explaintext=&format=json&cllimit=10&cldir=descending&titles=%s&redirects=" % match, headers={"user-agent": UA}).json()["query"]["pages"]
-            summaries = summaries[list(summaries.keys())[0]]
-            if not "missing" in summaries.keys():
-                summary = summaries["extract"].split('\n')[0]
-                send("\x02{}\x02: {}".format(summaries["title"],  summary))
+            send("\x02{}\x02: {}".format(*functions.get_wiki_summary(match[1], match[0])))
+
+
+@command("w", "wiki")
+def wikipedia(msginfo):
+    """.wiki <query> - searches Wikipedia for a query"""
+    query = " ".join(msginfo['msg'].split()[1:])
+    searchresult = requests.get("https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={}&format=json&srprop=redirecttitle&srprop=snippet".format(query) , headers={"user-agent": UA}).json()['query']['search'][0]
+    title = searchresult['title']
+    url = "https://en.wikipedia.org/wiki/{}".format(urllib.parse.quote(title))
+    send("{} | {}".format(url, functions.get_wiki_summary(title)[1]))
