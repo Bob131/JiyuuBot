3808159becdebacfab6c20aa8b41afed173dab23
Bob131 <bob@bob131.so>
2015-06-20 02:36:44 +1000

    Tweak version.py

--- modules/version.py
+++ modules/version.py
@@ -6,12 +6,15 @@ from . import command, send
 
 @command
 def version(msginfo):
-    """.version - print commit hash and time of the last git pull"""
+    """.version - print commit hash"""
     gitlogf = open(os.getcwd() + os.sep + ".git/logs/refs/remotes/origin/master", "r")
     gitlog = gitlogf.read()
     gitlogf.close()
     gitlog = gitlog.split("\n")[-2].split(" ")
     commit = gitlog[1][:7]
     timeg = int(gitlog[4])
-    vers = "http://github.com/Bob131/JiyuuBot - Commit hash: \x02%s\x02 - Last pull: %s (%s ago)" % (commit, time.strftime("%Y-%m-%d %H:%M:%S UTC", time.gmtime(timeg)), datetime.timedelta(seconds=math.floor(time.time()-timeg)))
+    vers = "Commit hash: \x02{}\x02".format(commit)
+    vers += " - Last updated: {}".format(time.strftime("%Y-%m-%d %H:%M:%S UTC", time.gmtime(timeg)))
+    vers += " ({} ago)".format(datetime.timedelta(seconds=math.floor(time.time()-timeg)))
+    send("http://github.com/Bob131/JiyuuBot")
     send(vers)
