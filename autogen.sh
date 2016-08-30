d9a4bdc3ae0d206c09519fff041f7566fe6d01ae
Bob131 <bob@bob131.so>
2016-08-30 13:47:55 +1000

    Small autogen.sh touchup
    This commit fixes some problems with the autogen.sh script, as
    identified by shellcheck[1].
    
    [1]: https://github.com/koalaman/shellcheck

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 _openbsd_acenv_set() {
-    [ -z ${AUTOCONF_VERSION:-} ] || return 0
+    [ -z "${AUTOCONF_VERSION:-}" ] || return 0
     export AUTOCONF_VERSION
-    AUTOCONF_VERSION="$( ls -1 /usr/local/bin/autoreconf-* | sort | tail -n 1 )"
+    AUTOCONF_VERSION="$( find /usr/local/bin/autoreconf-* | sort | tail -n 1 )"
     AUTOCONF_VERSION="${AUTOCONF_VERSION##*-}"
 }
 
 _openbsd_amenv_set() {
-    [ -z ${AUTOMAKE_VERSION:-} ] || return 0
+    [ -z "${AUTOMAKE_VERSION:-}" ] || return 0
     export AUTOMAKE_VERSION
-    AUTOMAKE_VERSION="$( ls -1 /usr/local/bin/automake-* | sort | tail -n 1 )"
+    AUTOMAKE_VERSION="$( find /usr/local/bin/automake-* | sort | tail -n 1 )"
     AUTOMAKE_VERSION="${AUTOMAKE_VERSION##*-}"
 }
 
 _openbsd_env_set() {
-    [ `uname` = OpenBSD ] || return 0
+    [ "$(uname)" = OpenBSD ] || return 0
     _openbsd_acenv_set
     _openbsd_amenv_set
 }
@@ -24,7 +24,7 @@ _openbsd_env_set
 
 set -e
 
-srcdir=`dirname $0`
+srcdir=$(dirname "$0")
 test -z "$srcdir" && srcdir=.
 
 aclocal --install
